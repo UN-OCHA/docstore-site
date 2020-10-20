ff2bf2ec9092b074a1053af4d548900e9ff057cc
Peter Droogmans (attiks) <peter@attiks.com>
2020-10-20 16:55:53 +0200

    Run script for travis

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,5 @@
-fin drush eval "_docstore_setup_testing()"
-./silk -test.v -silk.url http://docstore.local.docksal/api silk_create.md
+#fin drush eval "_docstore_setup_testing()"
+#./silk -test.v -silk.url http://docstore.local.docksal/api silk_create.md
 
+fin drush --uri=test eval "_docstore_setup_testing()"
+./silk -test.v -silk.url http://test.docstore.local.docksal/api silk_create.md
---
+++ tests/run_travis.sh
@@ -0,0 +1,3 @@
+fin drush eval "_docstore_setup_testing()"
+./silk -test.v -silk.url http://127.0.0.1/api silk_create.md
+
