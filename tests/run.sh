f289db2f1e7bfcb9fe97b50aa4a9f504b2c85939
Peter Droogmans (attiks) <peter@attiks.com>
2020-10-20 18:08:11 +0200

    Split run command

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,2 @@
-#fin drush eval "_docstore_setup_testing()"
-#./silk -test.v -silk.url http://docstore.local.docksal/api silk_create.md
-
-fin drush --uri=test eval "_docstore_setup_testing()"
-./silk -test.v -silk.url http://test.docstore.local.docksal/api silk_create.md
+fin drush eval "_docstore_setup_testing()"
+./silk -test.v -silk.url http://docstore.local.docksal/api silk_create.md
---
+++ tests/run_test.sh
@@ -0,0 +1,2 @@
+fin drush --uri=test eval "_docstore_setup_testing()"
+./silk -test.v -silk.url http://test.docstore.local.docksal/api silk_create.md
