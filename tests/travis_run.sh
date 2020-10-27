1c88ce32049dce3ef7fa33f014661e39fe9b78a1
Peter Droogmans (attiks) <peter@attiks.com>
2020-10-27 11:13:15 +0100

    Fix path

--- tests/travis_run.sh
+++ tests/travis_run.sh
@@ -1,12 +1,12 @@
 # Clear docstore, test vocabulary CRUD
-../../vendor/bin/drush eval "_docstore_setup_testing()"
-../../vendor/bin/drush cr
+../vendor/bin/drush eval "_docstore_setup_testing()"
+../vendor/bin/drush cr
 
 ./silk -test.v -silk.url http://127.0.0.1:8080/api silk_vocabulary_crud.md || exit 1;
 
 # Clear docstore, general tests
-../../vendor/bin/drush eval "_docstore_setup_testing()"
-../../vendor/bin/drush cr
+../vendor/bin/drush eval "_docstore_setup_testing()"
+../vendor/bin/drush cr
 
 # Add files
 (echo -n '{"filename":"test.pdf","mime":"application/pdf","data": "'; base64 ./files/test_xyzzy.pdf; echo '"}') | curl -X POST -H  "accept: application/json" -H  "API-KEY: abcd" -H "Content-Type: application/json" -d @-  http://docstore.local.docksal/api/files
