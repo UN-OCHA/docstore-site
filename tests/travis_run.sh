1b4d533477a2c31a619552460a64e38e053bbf2d
Peter Droogmans (attiks) <peter@attiks.com>
2020-10-27 11:21:29 +0100

    Post file to correct endpoint

--- tests/travis_run.sh
+++ tests/travis_run.sh
@@ -9,7 +9,7 @@
 ../vendor/bin/drush cr
 
 # Add files
-(echo -n '{"filename":"test.pdf","mime":"application/pdf","data": "'; base64 ./files/test_xyzzy.pdf; echo '"}') | curl -X POST -H  "accept: application/json" -H  "API-KEY: abcd" -H "Content-Type: application/json" -d @-  http://docstore.local.docksal/api/files
+(echo -n '{"filename":"test.pdf","mime":"application/pdf","data": "'; base64 ./files/test_xyzzy.pdf; echo '"}') | curl -X POST -H  "accept: application/json" -H  "API-KEY: abcd" -H "Content-Type: application/json" -d @-  http://127.0.0.1:8080/api/files
 
 ./silk -test.v -silk.url http://127.0.0.1:8080/api silk_create.md || exit 1;
 ./silk -test.v -silk.url http://127.0.0.1:8080/api silk_exceptions.md || exit 1;
