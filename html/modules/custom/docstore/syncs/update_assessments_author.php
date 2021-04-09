a502f22f8669129ad63a78c67c904212fa36476f
Peter Droogmans (attiks) <peter@attiks.com>
2021-04-09 10:17:29 +0200

    Update assessment author

---
+++ html/modules/custom/docstore/syncs/update_assessments_author.php
@@ -0,0 +1,53 @@
+<?php
+
+function updateAssessmentAuthor() {
+  $handle = fopen(__DIR__ . '/assessments_users.csv', 'r');
+
+  // First line is header.
+  $header = fgetcsv($handle, 0, ',', '"');
+  $header_lowercase = array_map('strtolower', $header);
+
+  foreach ($header_lowercase as $index => $field_name) {
+    $header_lowercase[$index] = trim($field_name);
+  }
+
+  $row_counter = 0;
+  while ($row = fgetcsv($handle, 0, ',', '"')) {
+    $row_counter++;
+
+    $data = [];
+    for ($i = 0; $i < count($row); $i++) {
+      $data[$header_lowercase[$i]] = trim($row[$i]);
+    }
+
+    print $row_counter . '. Processing document with id=' . $data['nid'] . "\n";
+
+    // Get assessment document using id.
+    $entities = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties([
+      'id' => $data['nid'],
+      'type' => 'assessment',
+    ]);
+
+    if (!empty($entities)) {
+      $document = reset($entities);
+    }
+    else {
+      print 'Document not found ' . $data['nid'] . "\n";
+      continue;
+    }
+
+    print $document->get('author')->value;
+    print $document->uuid() . "\n";
+
+    if (!$document->get('author')->isEmpty() && $document->get('author')->value !== 'AR') {
+      print "Skip document, already has contacts\n";
+    }
+
+    $document->set('author', $data['author']);
+    $document->save();
+  }
+
+  fclose($handle);
+}
+
+updateAssessmentAuthor();
