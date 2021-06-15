eff98524d3c6aa614a76447a3c30888fe236aadd
orakili <github@orakili.net>
2021-06-15 09:37:45 +0900

    [AR-249] Add checks on the publication and private statuses

--- html/modules/custom/docstore/src/Controller/DocumentController.php
+++ html/modules/custom/docstore/src/Controller/DocumentController.php
@@ -11,6 +11,7 @@ use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\File\FileSystem;
+use Drupal\Core\Http\Exception\CacheableNotFoundHttpException;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\ProxyClass\File\MimeType\MimeTypeGuesser;
 use Drupal\Core\State\State;
@@ -227,10 +228,7 @@ class DocumentController extends ControllerBase {
 
     // Load the document.
     $document = $this->loadDocument($id);
-
-    // Prepare the response data.
-    $data = $this->prepareEntityResourceDataForResponse($document, $provider);
-
+    
     // Add cache contexts and tags.
     $cache = $this->createResponseCache()
       ->addCacheTags(['documents'])
@@ -239,6 +237,19 @@ class DocumentController extends ControllerBase {
     if (isset($node_type)) {
       $cache->addCacheableDependency($node_type);
     }
+    
+    // Check access to the document: either public and published or provider is
+    // the owner.
+    if (!empty($document->private->value) || !$document->isPublished()) {
+      if (!$this->providerIsOwner($document, $provider, 'owner_id', FALSE)) {
+        throw new CacheableNotFoundHttpException($cache, strtr('@label does not exist', [
+          '@label' => $this->getResourceTypeLabel('node', FALSE),
+        ]));
+      }
+    }
+    
+    // Prepare the response data.
+    $data = $this->prepareEntityResourceDataForResponse($document, $provider);
 
     return $this->createCacheableJsonResponse($cache, $data, 200);
   }
